a7a7575add9ef280eff3ff2aa2d54c50126a4875
Sam Ramakrishnan <sramakrishn8@wisc.edu>
2020-03-12 17:28:08 -0500

    Added Common Utils file

---
+++ app/src/main/java/com/cs407/campusevents/CommonUtils.java
@@ -0,0 +1,12 @@
+package com.cs407.campusevents;
+
+import android.location.Location;
+
+public class CommonUtils {
+    public static String url = "blah blah";
+
+    public  static String getLocationName(Location j ){
+
+        return "";
+    }
+}
